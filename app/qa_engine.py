cac072aeaedbf79ebe0fbefef55d3f663b739699
Satournine <tunaorkun@outlook.com>
2025-05-16 22:49:02 +0300

    final

--- app/qa_engine.py
+++ app/qa_engine.py
@@ -1,10 +1,13 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
 from rag_pipeline import search_index
+import os
+
+hf_token = os.getenv("HF_TOKEN")
 
 model_name = "google/gemma-2b-it"
 
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForCausalLM.from_pretrained(model_name,
+tokenizer = AutoTokenizer.from_pretrained(model_name, hf_token)
+model = AutoModelForCausalLM.from_pretrained(model_name, hf_token,
                                              torch_dtype="auto")
 
 qa_pipeline = pipeline("text-generation", model=model, tokenizer=tokenizer, do_sample=True, max_new_tokens=512, temperature=0.7, top_p=0.9,repetition_penalty=1.1)
@@ -13,7 +16,7 @@ def format_prompt(context_chunks, question):
     context = "\n".join(context_chunks)
     prompt = (
             "You are a legal assistant. Use only the provided context below to answer the user's question.\n"
-       " – Do not use outside knowledge.\n"
+        " – Do not use outside knowledge.\n"
         "– Do not guess. If the context does not contain the answer, say: \"I'm not sure based on the current document.\"\n"
         "– Do not repeat the question or prompt in your answer.\n"
         "– Respond clearly and concisely.\n"
